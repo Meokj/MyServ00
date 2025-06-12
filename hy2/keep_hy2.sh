adb482a8c3cf334636148f756b4a7fea81d5f3e9
Meokj <153259973+Meokj@users.noreply.github.com>
2025-06-13 07:50:25 +0800

    Update keep_hy2.sh

--- hy2/keep_hy2.sh
+++ hy2/keep_hy2.sh
@@ -6,49 +6,6 @@ USERNAME=$(whoami)
 CONFIG_FILE=~/hysteria2/config.yaml
 CRONJOB="*/2 * * * * ~/hysteria2/check_process.sh"
 
-get_ports() {
-  UDP_PORT=""
-  TCP_PORT=""
-  udp_ports=($(devil port list | awk '$2=="udp"{print $1}'))
-  tcp_ports=($(devil port list | awk '$2=="tcp"{print $1}'))
-
-  if [[ ${#udp_ports[@]} -gt 1 ]]; then
-    UDP_PORT=${udp_ports[0]}
-    for ((i=1; i<${#udp_ports[@]}; i++)); do
-      devil port del udp "${udp_ports[i]}"
-    done
-  elif [[ ${#udp_ports[@]} -eq 1 ]]; then
-    UDP_PORT=${udp_ports[0]}  
-  else
-    while true; do
-      rand_udp_port=$(shuf -i 10000-65535 -n 1)
-      result=$(devil port add udp "$rand_udp_port" hy2 2>&1)
-      if [[ $result == *"Ok"* ]]; then
-        UDP_PORT=$rand_udp_port
-        break
-      fi
-    done
-  fi
-
-  if [[ ${#tcp_ports[@]} -gt 1 ]]; then
-    TCP_PORT=${tcp_ports[0]}
-    for ((i=1; i<${#tcp_ports[@]}; i++)); do
-      devil port del tcp "${tcp_ports[i]}"
-    done
-  elif [[ ${#tcp_ports[@]} -eq 1 ]]; then
-    TCP_PORT=${tcp_ports[0]}  
-  else
-    while true; do
-      rand_tcp_port=$(shuf -i 10000-65535 -n 1)
-      result=$(devil port add tcp "$rand_tcp_port" traffic 2>&1)
-      if [[ $result == *"Ok"* ]]; then
-        TCP_PORT=$rand_tcp_port
-        break
-      fi
-    done
-  fi
-}
-
 get_traffic_data() {
     response=$(curl -s -w "%{http_code}" -H "Authorization: ${PASSWORD}" http://127.0.0.1:${TCP_PORT}/traffic)
     http_code="${response: -3}"  
@@ -120,6 +77,49 @@ download() {
   fi
 
   chmod +x hysteria2
+}
+
+get_ports() {
+  UDP_PORT=""
+  TCP_PORT=""
+  udp_ports=($(devil port list | awk '$2=="udp"{print $1}'))
+  tcp_ports=($(devil port list | awk '$2=="tcp"{print $1}'))
+
+  if [[ ${#udp_ports[@]} -gt 1 ]]; then
+    UDP_PORT=${udp_ports[0]}
+    for ((i=1; i<${#udp_ports[@]}; i++)); do
+      devil port del udp "${udp_ports[i]}"
+    done
+  elif [[ ${#udp_ports[@]} -eq 1 ]]; then
+    UDP_PORT=${udp_ports[0]}  
+  else
+    while true; do
+      rand_udp_port=$(shuf -i 10000-65535 -n 1)
+      result=$(devil port add udp "$rand_udp_port" hy2 2>&1)
+      if [[ $result == *"Ok"* ]]; then
+        UDP_PORT=$rand_udp_port
+        break
+      fi
+    done
+  fi
+
+  if [[ ${#tcp_ports[@]} -gt 1 ]]; then
+    TCP_PORT=${tcp_ports[0]}
+    for ((i=1; i<${#tcp_ports[@]}; i++)); do
+      devil port del tcp "${tcp_ports[i]}"
+    done
+  elif [[ ${#tcp_ports[@]} -eq 1 ]]; then
+    TCP_PORT=${tcp_ports[0]}  
+  else
+    while true; do
+      rand_tcp_port=$(shuf -i 10000-65535 -n 1)
+      result=$(devil port add tcp "$rand_tcp_port" traffic 2>&1)
+      if [[ $result == *"Ok"* ]]; then
+        TCP_PORT=$rand_tcp_port
+        break
+      fi
+    done
+  fi
 }
 
 generate_configuration() {
@@ -232,9 +232,9 @@ EOF
 }
 
 get_ip
+get_ports
 check
 download
-get_ports
 generate_configuration
 run_hysteria2
 scheduled_task
