b5ff25aca38f5b0fcb39663824a03021a2522c1c
Meokj <153259973+Meokj@users.noreply.github.com>
2025-06-13 12:07:53 +0800

    Update install_hy2.sh

--- hy2/install_hy2.sh
+++ hy2/install_hy2.sh
@@ -152,7 +152,7 @@ get_links() {
 hysteria2://***@$IP:$UDP_PORT/?sni=www.bing.com&alpn=h3&insecure=1#${ISP}-${NUMBER}-${USERNAME}
 EOF
   echo
-  echo "$hysteria2节点信息如下："
+  echo "复制以下节点信息并修改密码，格式：username:password"
   cat list.txt
   echo
 
